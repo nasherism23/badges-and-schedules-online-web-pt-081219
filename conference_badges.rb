81ba4eaf025618508dcfeac0b93a2a7b5fe47345
James Nash <nash.jamesm@gmail.com>
2019-08-20 14:33:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,5 +15,17 @@ def batch_badge_creator (attendees)
 end 
 
 def assign_rooms (attendees)
-  attendees.each do |attendee|
-    return 
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
